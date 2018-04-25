208a432d1d7c48a215cbd790b29805254b1a0805
CrossroadsMan <crossroadsman@gmail.com>
2018-04-24 21:10:59 -0600

    Pep8 for file_manager

--- file_manager.py
+++ file_manager.py
@@ -15,10 +15,10 @@ class FileManager:
                     file_data.append(line)
                 file_handle.close()
         return file_data
-    
+
     def save_file(self, file_data, filename):
         file_handle = open(filename, 'a')
-        file_handle.write('hello, world') # do we need to add a trailing newline?
+        file_handle.write('hello, world')  # trailing newline req'd?
         file_handle.close()
 
 
@@ -32,7 +32,6 @@ if __name__ == "__main__":
     for line in file_data:
         print(line)
 
-    
     print("test2: file write")
     file_manager = FileManager()
     file_manager.save_file("dummy data", 'test.txt')
